85d22cd30b3c6f00a734a218c618cffe24506d1a
julsCadenas <jscadenas12@gmail.com>
2024-09-04 13:54:03 +0800

    npm run build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './styles/App.css'
-import Index from './pages/index/index'
+import Index from './pages/index'
 import SpecificProj from './pages/specificproj';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
