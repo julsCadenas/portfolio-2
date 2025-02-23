b566f30f071b68b1c8b93db51db4685cfcf45e20
julsCadenas <jscadenas12@gmail.com>
2025-02-23 16:03:44 +0800

    removed footer on about me page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Terminal from '../components/terminal'
 import Header from '../components/header'
-// import Contact from '../components/contact'
 
 const AboutMe = () => {
   return (
     <main className='flex items-center justify-center flex-col min-h-screen'>
         <Header />
         <Terminal />
-        {/* <Contact /> */}
     </main>
   )
 }
